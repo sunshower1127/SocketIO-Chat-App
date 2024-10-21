31e18723db4fc49278260c09520771ab5246bd2f
Sunwoo Kim (Mac) <sunshower1127@gmail.com>
2024-10-21 13:25:51 +0900

    socket.io의 Room

--- src/server.js
+++ src/server.js
@@ -1,43 +1,27 @@
 import express from "express";
 import http from "http";
-import path from "path";
 import SocketIO from "socket.io";
 
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-
-app.use("/public", express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome");
-  });
-
-  socket.on("offer", (offer, roomName) => {
-    socket.to(roomName).emit("offer", offer);
-  });
-
-  socket.on("answer", (answer, roomName) => {
-    socket.to(roomName).emit("answer", answer);
-  });
-
-  socket.on("ice", (ice, roomName) => {
-    socket.to(roomName).emit("ice", ice);
   });
 });
 
-httpServer.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
